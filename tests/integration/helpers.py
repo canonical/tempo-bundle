016461a8bfbdc39c50d6aa605d0ec9c3562604c4
michael <michael.dmitry98@gmail.com>
2024-07-09 18:53:57 +0300

    add full path test

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -1,14 +1,88 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 import inspect
+import json
 import logging
 import os
 from pathlib import Path
 from typing import List, Union
 
+import requests
+from juju.application import Application
+from juju.unit import Unit
+from minio import Minio
 from pytest_operator.plugin import OpsTest
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 logger = logging.getLogger(__name__)
+MINIO = "minio"
+BUCKET_NAME = "tempo"
+S3_INTEGRATOR = "s3-integrator"
+ACCESS_KEY = "accesskey"
+SECRET_KEY = "secretkey"
+
+
+async def get_unit_address(ops_test: OpsTest, app_name, unit_no):
+    status = await ops_test.model.get_status()
+    app = status["applications"][app_name]
+    if app is None:
+        assert False, f"no app exists with name {app_name}"
+    unit = app["units"].get(f"{app_name}/{unit_no}")
+    if unit is None:
+        assert False, f"no unit exists in app {app_name} with index {unit_no}"
+    return unit["address"]
+
+
+async def deploy_and_configure_minio(ops_test: OpsTest):
+    config = {
+        "access-key": ACCESS_KEY,
+        "secret-key": SECRET_KEY,
+    }
+    await ops_test.model.deploy(MINIO, channel="edge", trust=True, config=config)
+    await ops_test.model.wait_for_idle(apps=[MINIO], status="active", timeout=2000)
+    minio_addr = await get_unit_address(ops_test, MINIO, "0")
+
+    mc_client = Minio(
+        f"{minio_addr}:9000",
+        access_key="accesskey",
+        secret_key="secretkey",
+        secure=False,
+    )
+
+    # create tempo bucket
+    found = mc_client.bucket_exists(BUCKET_NAME)
+    if not found:
+        mc_client.make_bucket(BUCKET_NAME)
+
+    # configure s3-integrator
+    s3_integrator_app: Application = ops_test.model.applications[S3_INTEGRATOR]
+    s3_integrator_leader: Unit = s3_integrator_app.units[0]
+
+    await s3_integrator_app.set_config(
+        {
+            "endpoint": f"http://{minio_addr}:9000",
+            "bucket": BUCKET_NAME,
+        }
+    )
+
+    action = await s3_integrator_leader.run_action("sync-s3-credentials", **config)
+    action_result = await action.wait()
+    assert action_result.status == "completed"
+
+
+def get_traces(tempo_host: str, service_name, is_tls=False):
+    req = requests.get(
+        "https://" if is_tls else "http://" + tempo_host + ":3200/api/search",
+        params={"service.name": service_name},
+        verify=False,
+    )
+    assert req.status_code == 200
+    return json.loads(req.text)["traces"]
+
+
+@retry(stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=4, max=10))
+async def get_traces_patiently(tempo_host, service_name, is_tls):
+    return get_traces(tempo_host, service_name, is_tls)
 
 
 def get_this_script_dir() -> Path:
@@ -35,6 +109,7 @@ async def cli_deploy_bundle(
         name,
     ] + overlay_args
 
+    print("ARGS ", run_args)
     retcode, stdout, stderr = await ops_test.run(*run_args)
     assert retcode == 0, f"Deploy failed: {(stderr or stdout).strip()}"
-    logger.info(stdout)
+    logger.info("Output %s", stdout)
--- tests/integration/test_bundle.py
+++ tests/integration/test_bundle.py
@@ -6,10 +6,18 @@
 import logging
 
 import pytest
-from helpers import cli_deploy_bundle
+from helpers import (
+    S3_INTEGRATOR,
+    cli_deploy_bundle,
+    deploy_and_configure_minio,
+    get_traces_patiently,
+    get_unit_address,
+)
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
+
+COORDINATOR = "tempo"
 
 
 @pytest.mark.abort_on_fail
@@ -19,7 +27,10 @@ async def test_build_and_deploy(ops_test: OpsTest, rendered_bundle):
     Assert on the unit status before any relations/configurations take place.
     """
     await cli_deploy_bundle(ops_test, str(rendered_bundle))
-    await ops_test.model.wait_for_idle(status="active", timeout=1000, idle_period=90)
+    # they will remain on blocked, as s3 integrator didn't provide tempo with an endpoint yet.
+    await ops_test.model.wait_for_idle(
+        apps=[S3_INTEGRATOR, COORDINATOR], status="blocked", timeout=1000, idle_period=90
+    )
 
 
 @pytest.mark.abort_on_fail
@@ -39,6 +50,31 @@ async def test_deployment_layout(ops_test: OpsTest, expected_layout):
 
 
 @pytest.mark.abort_on_fail
+async def test_deployments_active(ops_test: OpsTest):
+    """
+    Configure an s3-compatible charm for Tempo to use as storage.
+    Assert that after successful configurations, all workloads are in active/idle state.
+    """
+
+    await deploy_and_configure_minio(ops_test)
+    await ops_test.model.wait_for_idle(status="active", timeout=1000, idle_period=90)
+
+
+@pytest.mark.abort_on_fail
+async def test_ingested_charm_traces(ops_test: OpsTest):
+    """
+    Test traces are getting injested into Tempo.
+    """
+    # adjust update-status interval to generate a charm tracing span faster
+    await ops_test.model.set_config({"update-status-hook-interval": "30s"})
+    coordinator_addr = await get_unit_address(ops_test, COORDINATOR, "0")
+    assert await get_traces_patiently(coordinator_addr, "tempo-charm", False)
+
+    # adjust back to the default interval time
+    await ops_test.model.set_config({"update-status-hook-interval": "5m"})
+
+
+@pytest.mark.abort_on_fail
 async def test_tls_overlay(ops_test: OpsTest, rendered_bundle):
     """
     Deploy the tempo bundle with tls overlay.
@@ -49,4 +85,15 @@ async def test_tls_overlay(ops_test: OpsTest, rendered_bundle):
     await ops_test.model.wait_for_idle(status="active", timeout=1000, idle_period=90)
 
 
-# TODO: add tests for ingested traces
+@pytest.mark.abort_on_fail
+async def test_ingested_charm_traces_tls(ops_test: OpsTest):
+    """
+    Test traces are getting injested into Tempo while TLS is enabled.
+    """
+    # adjust update-status interval to generate a charm tracing span faster
+    await ops_test.model.set_config({"update-status-hook-interval": "30s"})
+    coordinator_addr = await get_unit_address(ops_test, COORDINATOR, "0")
+    assert get_traces_patiently(coordinator_addr, "tempo-charm", True)
+
+    # adjust back to the default interval time
+    await ops_test.model.set_config({"update-status-hook-interval": "5m"})
